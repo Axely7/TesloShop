a25ccb90b13b755ac654d51d4f1a7c364776b423
Axely7 <axely7@hotmail.com>
2022-07-28 08:44:08 -0500

    Navbar Created

---
+++ components/layouts/ShopLayout.tsx
@@ -0,0 +1,41 @@
+import Head from "next/head";
+import { FC } from "react";
+import { Navbar } from "../ui";
+
+interface Props {
+  title: string;
+  pageDescription: string;
+  imageFullUrl?: string;
+  children: any;
+}
+
+export const ShopLayout: FC<Props> = ({
+  children,
+  title,
+  pageDescription,
+  imageFullUrl,
+}) => {
+  return (
+    <>
+      <Head>
+        <title>{title}</title>
+        <meta name="description" content={pageDescription} />
+        <meta name="og:title" content={title} />
+
+        {imageFullUrl && <meta name="og:image" content={imageFullUrl} />}
+      </Head>
+
+      <nav>
+        <Navbar />
+      </nav>
+
+      <main
+        style={{ margin: "80px auto", maxWidth: "1440px", padding: "0px 30px" }}
+      >
+        {children}
+      </main>
+
+      <footer></footer>
+    </>
+  );
+};
