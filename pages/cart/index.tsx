30a6a8a5e31e694034be437faf446db7395b3c91
Axely7 <axely7@hotmail.com>
2022-08-03 08:59:13 -0500

    checkout address form

---
+++ pages/cart/index.tsx
@@ -0,0 +1,46 @@
+import {
+  Button,
+  Card,
+  CardContent,
+  Divider,
+  Grid,
+  Typography,
+} from "@mui/material";
+import { Box } from "@mui/system";
+import React from "react";
+import { CartList, OrderSummary } from "../../components/cart";
+import { ShopLayout } from "../../components/layouts";
+
+const CartPage = () => {
+  return (
+    <ShopLayout
+      title="Carrito - 3"
+      pageDescription="Carrito de compras de la tienda"
+    >
+      <Typography variant="h1" component="h1">
+        Carrito
+      </Typography>
+      <Grid container>
+        <Grid item xs={12} sm={7}>
+          <CartList editable />
+        </Grid>
+        <Grid item xs={12} sm={5}>
+          <Card className="summary-card">
+            <CardContent>
+              <Typography variant="h2">Orden</Typography>
+              <OrderSummary />
+              <Divider sx={{ my: 1 }} />
+              <Box sx={{ mt: 3 }}>
+                <Button color="secondary" className="circular-btn" fullWidth>
+                  Checkout
+                </Button>
+              </Box>
+            </CardContent>
+          </Card>
+        </Grid>
+      </Grid>
+    </ShopLayout>
+  );
+};
+
+export default CartPage;
