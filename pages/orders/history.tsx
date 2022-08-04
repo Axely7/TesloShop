83979e4854eb97716551a1a1895c66b22979c1ab
Axely7 <axely7@hotmail.com>
2022-08-04 08:47:07 -0500

    data grid install

---
+++ pages/orders/history.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { ShopLayout } from "../../components/layouts";
+
+const HistoryPage = () => {
+  return (
+    <ShopLayout
+      title="Historial de ordenes"
+      pageDescription="Historial de ordenes del clientes"
+    ></ShopLayout>
+  );
+};
+
+export default HistoryPage;
