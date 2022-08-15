7a099a869c0e2e0e73242fd8b70d0f7b85032372
Axely7 <axely7@hotmail.com>
2022-08-15 08:43:10 -0500

    get product by slug endpoint

---
+++ pages/api/products/[slug].ts
@@ -0,0 +1,36 @@
+import type { NextApiRequest, NextApiResponse } from 'next'
+import { db } from '../../../database';
+import { IProduct } from '../../../interfaces';
+import Product from '../../../models/Product';
+
+type Data = | {
+    message: string
+} | IProduct
+
+export default function handler(req: NextApiRequest, res: NextApiResponse<Data>) {
+   switch (req.method) {
+    case 'GET':
+        
+        return getProductBySlug(req, res);
+   
+    default:
+        return res.status(400).json({
+            message: 'Bad request'
+        })
+   }
+}
+
+const getProductBySlug = async(req: NextApiRequest, res: NextApiResponse<Data>) => {
+    const { slug } = req.query;
+
+    await db.connect();
+    const productToGet = await Product.findOne({slug}).lean();
+
+    if(!productToGet){
+        await db.disconnect();
+        return res.status(404).json({message: 'No hay producto con ese Slug' + slug});
+    }
+
+    await db.disconnect();
+    return res.status(200).json(productToGet!)
+}
