b09bd374f0a0813f8a3fd6c4174657e0854c39c6
Axely7 <axely7@hotmail.com>
2022-10-17 08:58:20 -0500

    validations in login form

--- pages/auth/login.tsx
+++ pages/auth/login.tsx
@@ -2,45 +2,91 @@ import NextLink from "next/link";
 import { Box, Button, Grid, Link, TextField, Typography } from "@mui/material";
 import React from "react";
 import { AuthLayout } from "../../components/layouts";
+import { useForm } from "react-hook-form";
+import { validations } from "../../utils";
+import { tesloApi } from "../../api";
+
+type FormData = {
+  email: string;
+  password: string;
+};
 
 const LoginPage = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<FormData>();
+
+  console.log(errors);
+
+  const onLoginUser = async ({ email, password }: FormData) => {
+    try {
+      const { data } = await tesloApi.post("/user/login", { email, password });
+      const { token, user } = data;
+      console.log({ token, user });
+    } catch (error) {
+      console.log("Error en las credenciales");
+    }
+  };
+
   return (
     <AuthLayout title={"Ingresar"}>
-      <Box sx={{ width: 350, padding: "10px 20px" }}>
-        <Grid container spacing={2}>
-          <Grid item xs={12}>
-            <Typography variant="h1" component="h1">
-              Iniciar Sesión
-            </Typography>
+      <form onSubmit={handleSubmit(onLoginUser)} noValidate>
+        <Box sx={{ width: 350, padding: "10px 20px" }}>
+          <Grid container spacing={2}>
+            <Grid item xs={12}>
+              <Typography variant="h1" component="h1">
+                Iniciar Sesión
+              </Typography>
+            </Grid>
+            <Grid item xs={12}>
+              <TextField
+                type="email"
+                label="Correo"
+                variant="filled"
+                fullWidth
+                {...register("email", {
+                  required: "Este campo es requerido",
+                  validate: validations.isEmail,
+                })}
+                error={!!errors.email}
+                helperText={errors.email?.message}
+              />
+            </Grid>
+            <Grid item xs={12}>
+              <TextField
+                label="Contraseña"
+                type="password"
+                variant="filled"
+                fullWidth
+                {...register("password", {
+                  required: "Este campo es requerido",
+                  minLength: { value: 6, message: "Mínimo 6 caracteres" },
+                })}
+                error={!!errors.password}
+                helperText={errors.password?.message}
+              />
+            </Grid>
+            <Grid item xs={12}>
+              <Button
+                type="submit"
+                color="secondary"
+                className="circular-btn"
+                size="large"
+                fullWidth
+              >
+                Ingresar
+              </Button>
+            </Grid>
+            <Grid item xs={12} display="flex" justifyContent="end">
+              <NextLink href="/auth/register" passHref>
+                <Link underline="always">¿No tienes cuenta?</Link>
+              </NextLink>
+            </Grid>
           </Grid>
-          <Grid item xs={12}>
-            <TextField label="Correo" variant="filled" fullWidth />
-          </Grid>
-          <Grid item xs={12}>
-            <TextField
-              label="Contraseña"
-              type="password"
-              variant="filled"
-              fullWidth
-            />
-          </Grid>
-          <Grid item xs={12}>
-            <Button
-              color="secondary"
-              className="circular-btn"
-              size="large"
-              fullWidth
-            >
-              Ingresar
-            </Button>
-          </Grid>
-          <Grid item xs={12} display="flex" justifyContent="end">
-            <NextLink href="/auth/register" passHref>
-              <Link underline="always">¿No tienes cuenta?</Link>
-            </NextLink>
-          </Grid>
-        </Grid>
-      </Box>
+        </Box>
+      </form>
     </AuthLayout>
   );
 };
