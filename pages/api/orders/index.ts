d5cca69f5a923c123e8de93df0b721e375f7a2f3
Axel Jimenez <axely7@hotmail.com>
2022-11-24 08:44:04 -0600

    order created and verified from backend

--- pages/api/orders/index.ts
+++ pages/api/orders/index.ts
@@ -1,9 +1,14 @@
 import type { NextApiRequest, NextApiResponse } from 'next'
+import { getSession } from 'next-auth/react';
 import { json } from 'stream/consumers';
+import { db } from '../../../database';
+import { IOrder } from '../../../interfaces';
+import { Product, Order } from '../../../models';
+
 
 type Data = {
     message: string
-}
+} | IOrder
 
 export default function handler (req: NextApiRequest, res: NextApiResponse<Data>) {
     switch (req.method) {
@@ -20,8 +25,60 @@ export default function handler (req: NextApiRequest, res: NextApiResponse<Data>
 
 const createOrder = async (req: NextApiRequest, res: NextApiResponse<Data>) => {
 
-    const body = req.body;
-    console.log(body)
+    const { orderItems, total } = req.body as IOrder;
 
-    return res.status(201).json(body)
+    // Verificar que tengamos un usuario
+    const session: any = await getSession({req})
+
+    if(!session){
+        return res.status(401).json({message: 'Debe de estar autenticado para realizar esto'})
+    }
+
+    // Crear arreglo con los productos que la persona quiere
+    const productsIds = orderItems.map(product => product._id);
+    await db.connect();
+
+    const dbProducts = await Product.find({_id: { $in: productsIds }});
+
+    try {
+        const subTotal = orderItems.reduce(
+            (prev, current) => {
+                const currentPrice = dbProducts.find(prod => prod.id === current._id)?.price;
+                if(!currentPrice){
+                    throw new Error("Verifique el carrito de nuevo, producto no existe");
+                    
+                }
+
+
+                return (currentPrice * current.quantity + prev)
+            },
+            0
+          );
+
+
+        const taxRate = Number(process.env.NEXT_PUBLIC_TAX_RATE || 0);
+        const backendTotal = subTotal * (taxRate + 1);
+
+        if(total !== backendTotal){
+            throw new Error("El total no cuadra con el monto");
+        }
+
+        // Todo bien hasta este punto
+        const userId = session.user._id;
+        const newOrder = new Order({...req.body, isPaid: false, user: userId});
+        await newOrder.save();
+
+        return res.status(201).json(newOrder)
+
+    } catch (error: any) {
+        await db.disconnect();
+        console.log(error)
+        res.status(400).json({
+            message: error.message || 'Revise logs del servidor'
+        })
+        
+    }
+   
+
+    return res.status(201).json(req.body)
 }
