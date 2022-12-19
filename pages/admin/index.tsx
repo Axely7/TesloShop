7f263bbf2d777390d305d4ab07640ad8626e272b
Axel Jimenez <axely7@hotmail.com>
2022-12-19 11:56:34 -0600

    api admin dashboard created and showing in dashboard every 30 seconds

--- pages/admin/index.tsx
+++ pages/admin/index.tsx
@@ -1,21 +1,60 @@
 import { AccessTimeOutlined, AttachMoneyOutlined, CancelPresentationOutlined, CategoryOutlined, CreditCardOffOutlined, CreditCardOutlined, DashboardOutlined, GroupOutlined, ProductionQuantityLimitsOutlined } from '@mui/icons-material'
-import { Grid } from '@mui/material'
-import React from 'react'
+import { Grid, Typography } from '@mui/material'
+import React, { useEffect, useState } from 'react'
+import useSWR from 'swr'
 import { SummaryTile } from '../../components/admin/SummaryTile'
 import { AdminLayout } from '../../components/layouts'
+import { DashboardSummaryResponse } from '../../interfaces'
 
 const DashboardPage = () => {
+
+  const { data, error } = useSWR<DashboardSummaryResponse>('/api/admin/dashboard', {
+    refreshInterval: 30 * 1000
+  });
+
+  const [refreshIn, setRefreshIn] = useState(30)
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      console.log('Tick')
+      setRefreshIn(refreshIn => refreshIn > 0 ? refreshIn - 1 : 30)
+    }, 1000)
+  
+    return () => clearInterval(interval)
+  }, [])
+  
+
+  if(!error && !data){
+    return <></>
+  }
+
+  if(error){
+    console.log(error)
+    return <Typography>Error al cargar la información</Typography>
+  }
+
+  const {
+    numberOfOrders,
+    paidOrders,
+    numberOfClients,
+    numberOfProducts,
+    productsWithNoInventory,
+    lowInventory,
+    notPaidOrders,
+  } = data!;
+
+
   return (
    <AdminLayout title='Dashboard' subTitle='Estadisticas Generales' icon={<DashboardOutlined />}>
       <Grid container spacing={2}>
-        <SummaryTile title={1} subTitle="Ordenes totales" icon={<CreditCardOutlined color='secondary' sx={{fontSize: 40}} />}/>
-        <SummaryTile title={2} subTitle="Ordenes pagadas" icon={<AttachMoneyOutlined color='success' sx={{fontSize: 40}} />}/>
-        <SummaryTile title={3} subTitle="Ordenes pendientes" icon={<CreditCardOffOutlined color='error' sx={{fontSize: 40}} />}/>
-        <SummaryTile title={4} subTitle="Clientes" icon={<GroupOutlined color='primary' sx={{fontSize: 40}} />}/>
-        <SummaryTile title={5} subTitle="Productos" icon={<CategoryOutlined color='warning' sx={{fontSize: 40}} />}/>
-        <SummaryTile title={6} subTitle="Sin existencias" icon={<CancelPresentationOutlined color='error' sx={{fontSize: 40}} />}/>
-        <SummaryTile title={7} subTitle="Bajo inventario" icon={<ProductionQuantityLimitsOutlined color='warning' sx={{fontSize: 40}} />}/>
-        <SummaryTile title={8} subTitle="Actualización en:" icon={<AccessTimeOutlined color='secondary' sx={{fontSize: 40}} />}/>
+        <SummaryTile title={numberOfOrders} subTitle="Ordenes totales" icon={<CreditCardOutlined color='secondary' sx={{fontSize: 40}} />}/>
+        <SummaryTile title={paidOrders} subTitle="Ordenes pagadas" icon={<AttachMoneyOutlined color='success' sx={{fontSize: 40}} />}/>
+        <SummaryTile title={notPaidOrders} subTitle="Ordenes pendientes" icon={<CreditCardOffOutlined color='error' sx={{fontSize: 40}} />}/>
+        <SummaryTile title={numberOfClients} subTitle="Clientes" icon={<GroupOutlined color='primary' sx={{fontSize: 40}} />}/>
+        <SummaryTile title={numberOfProducts} subTitle="Productos" icon={<CategoryOutlined color='warning' sx={{fontSize: 40}} />}/>
+        <SummaryTile title={productsWithNoInventory} subTitle="Sin existencias" icon={<CancelPresentationOutlined color='error' sx={{fontSize: 40}} />}/>
+        <SummaryTile title={lowInventory} subTitle="Bajo inventario" icon={<ProductionQuantityLimitsOutlined color='warning' sx={{fontSize: 40}} />}/>
+        <SummaryTile title={refreshIn} subTitle="Actualización en:" icon={<AccessTimeOutlined color='secondary' sx={{fontSize: 40}} />}/>
       </Grid>
    </AdminLayout>
   )
