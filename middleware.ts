e9eb27f39dcce7f127234f812038483c460a4984
Axel Jimenez <axely7@hotmail.com>
2022-12-20 09:40:40 -0600

    middlewares implemented to api and admin dashboard

--- middleware.ts
+++ middleware.ts
@@ -30,19 +30,44 @@ export async function middleware(req: NextRequest){
     // }
 
 
-  const session = await getToken({ req, secret: process.env.NEXTAUTH_SECRET });
+  const session: any = await getToken({ req, secret: process.env.NEXTAUTH_SECRET });
     console.log(session)
-  if (!session) {
+
+  if (!session) {    
+    
+    if(req.nextUrl.pathname.startsWith('/api/admin')){
+      return NextResponse.redirect(new URL('/api/auth/unauthorized', req.url))
+      
+    }
+
     const requestedPage = req.nextUrl.pathname;
     const url = req.nextUrl.clone();
     url.pathname = `/auth/login`;
     url.search = `p=${requestedPage}`;
-    return NextResponse.redirect(url);
+
+    // return NextResponse.redirect(url);
+    // const requestedPage = req.nextUrl.pathname;
+    // return NextResponse.redirect(new URL(`/auth/login?p=${requestedPage}`, req.url))
+
   }
- 
+
+    const validRoles = ['admin'];
+
+    if(req.nextUrl.pathname.startsWith('/admin')){
+      if(!validRoles.includes(session.user.role)){
+        return NextResponse.redirect(new URL("/", req.url))
+      }
+    }
+
+    if(req.nextUrl.pathname.startsWith('/api/admin')){
+      if(!validRoles.includes(session.user.role)){
+        return NextResponse.redirect(new URL(`/api/auth/unauthorized`, req.url))
+      }
+    }
+
   return NextResponse.next();
 }
 
 export const config = {
-    matcher: ['/checkout/address', '/checkout/summary']
+    matcher: ['/checkout/address', '/checkout/summary', '/admin/:path*', '/api/admin/:path*']
 }
