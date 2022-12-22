ab0f79d0881abb29f04da5bd6203033c3516aaf5
Axel Jimenez <axely7@hotmail.com>
2022-12-22 10:03:45 -0600

    changing user roles

--- pages/admin/users.tsx
+++ pages/admin/users.tsx
@@ -1,25 +1,74 @@
 import { PeopleOutline } from '@mui/icons-material';
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { AdminLayout } from '../../components/layouts/AdminLayout';
 import { DataGrid, GridColDef, GridValueGetterParams } from "@mui/x-data-grid";
-import { Grid } from '@mui/material';
+import { Grid, MenuItem, Select } from '@mui/material';
 import useSWR from 'swr';
 import { IUser } from '../../interfaces';
+import tesloApi from '../../api/tesloApi';
 
 const UsersPage = () => {
 
-
     const { data, error } = useSWR<IUser[]>('/api/admin/users')
+    const [users, setUsers] = useState<IUser[]>([])
+
+    useEffect(() => {
+      if(data){
+        setUsers(data)
+      }      
+    }, [data])
+    
 
     if(!data && !error) return (<></>)
+
+    const onRoleUpdated = async (userId: string, newRole: string) => {
+
+        const previousUser = users.map(user => ({...user}))
+
+        const updatedUsers = users.map(user => ({
+            ...user,
+            role: userId === user._id ? newRole : user.role
+        }))
+
+        setUsers(updatedUsers)
+
+
+        try {
+            await tesloApi.put('/admin/users', { userId, role: newRole });
+        } catch (error) {
+            setUsers(previousUser)
+            console.log(error)
+            alert('No se pudo actualizar el role del usuario');
+        }
+    }
 
     const columns: GridColDef[] = [
         {field: 'email', headerName: 'Correo', width: 250},
         {field: 'name', headerName: 'Nombre Completo', width: 300},
-        {field: 'role', headerName: 'Rol', width: 300}
+        {
+            field: 'role', 
+            headerName: 'Rol', 
+            width: 300,
+            renderCell: ({row}: GridValueGetterParams) => {
+                return (
+                    <Select
+                        value={row.role}
+                        label="Rol"
+                        onChange={({target}) => onRoleUpdated(row.id, target.value)}
+                        sx={{width: '300px'}}
+                    >
+                        <MenuItem value='admin'>Admin</MenuItem>
+                        <MenuItem value='client'>Client</MenuItem>
+                        <MenuItem value='super-user'>Super User</MenuItem>
+                        <MenuItem value='CEO'>SEO</MenuItem>
+
+                    </Select>
+                )
+            }
+        },
     ]
 
-    const rows = data!.map(user => ({
+    const rows = users.map(user => ({
         id: user._id,
         email: user.email,
         name: user.name,
